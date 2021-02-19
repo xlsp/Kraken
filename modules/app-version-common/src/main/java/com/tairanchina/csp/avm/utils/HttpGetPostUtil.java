58f2058eb1feac054442038d2b4fb520f551e4cf
touchid <ios.7@qq.com>
2021-02-19 17:14:11 +0800

    init

---
+++ modules/app-version-common/src/main/java/com/tairanchina/csp/avm/utils/HttpGetPostUtil.java
@@ -0,0 +1,66 @@
+package com.tairanchina.csp.avm.utils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class HttpGetPostUtil {
+    protected static final int SOCKET_TIMEOUT = 10000; // 10S
+    //  protected static final String GET = "GET";
+
+    public static String get(String host, Map<String, String> params) {
+        String text = null;
+
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //HttpClientBuilder.create().build();
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet("" + host);
+        //httpUrlConn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        //httpGet.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");  // 设置请求头消息User-Agent
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            if (responseEntity != null) {
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + EntityUtils.toString(responseEntity));
+                text = String.valueOf(responseEntity.getContentLength());
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return text;
+
+    }
+
+
+}
---
+++ modules/example/src/main/java/com/opensesame/config/Config.java
@@ -0,0 +1,87 @@
+package com.opensesame.config;
+
+import com.mars.common.base.config.MarsConfig;
+import com.mars.common.base.config.model.FileUploadConfig;
+import com.mars.common.base.config.model.RequestConfig;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * 更多配置项，请到官网查看官方文档
+ */
+public class Config extends MarsConfig {
+
+    /**
+     * 限制上传文件的大小
+     *
+     * @return
+     */
+    @Override
+    public FileUploadConfig fileUploadConfig() {
+        FileUploadConfig fileUploadConfig = new FileUploadConfig();
+        fileUploadConfig.setFileSizeMax(fileUploadConfig.getFileSizeMax() * 100);
+        fileUploadConfig.setSizeMax(fileUploadConfig.getSizeMax() * 100);
+        return fileUploadConfig;
+    }
+
+    @Override
+    public RequestConfig requestConfig() {
+        RequestConfig requestConfig = new RequestConfig();
+        requestConfig.setReadTimeout(10000);
+        requestConfig.setReadSize(1 * 1024 * 1024);
+        return requestConfig;
+    }
+
+    // 端口号，默认8080
+    @Override
+    public int port() {
+        return super.port();
+    }
+//    // 跨域配置，默认可以跨域
+//    @Override
+//    public CrossDomainConfig crossDomainConfig() {
+//        return super.crossDomainConfig();
+//    }
+//    /**
+//     * redis连接
+//     * @return
+//     */
+//    @Override
+//    public JedisConfig jedisConfig() {
+//        JedisConfig jedisConfig = new JedisConfig();
+//        jedisConfig.setHost("127.0.0.1");
+//        jedisConfig.setPort(6379);
+//        jedisConfig.setDatabase(5);
+//
+//        // 实际场景下，这句最好配一下，根据具体情况进行连接池的参数配置
+//        // jedisConfig.setJedisPoolConfig();
+//
+//        return jedisConfig;
+//    }
+
+    /**
+     * 配置数据源
+     *
+     * @return
+     */
+    @Override
+    public List<Properties> jdbcProperties() {
+        List<Properties> list = new ArrayList<>();
+
+        // 用的是阿里巴巴的 druid数据源，其他属性可自行查阅
+        Properties properties = new Properties();
+        properties.put("name", "dataSource");
+        properties.put("url", "");
+        properties.put("username", "");
+        properties.put("password", "");
+        properties.put("driverClassName", "com.mysql.jdbc.Driver");
+
+        list.add(properties);
+
+        // 如果要多个数据源，add多个到list即可
+
+        return list;
+    }
+}
